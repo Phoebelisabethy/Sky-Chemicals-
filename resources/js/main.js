24f5133a39d3cd37f2fd811d458b296eff69dadc
Phoebelisabethy <phoebe.hurst@hotmail.com>
2018-05-10 09:47:20 +0100

    Progress made to colour activation

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,23 +2,13 @@ $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 3000,
     arrows: false,
     draggable: false,
     pauseOnFocus: false,
-    pauseOnHover: false
+    pauseOnHover: false,
+    fade: true,
   });
 
-  var lastScrollTop = 0;
-  $(window).scroll(function(){
-    var scrollTop = $(this).scrollTop();
-    if (scrollTop - lastScrollTop > 50){
-      var navHeight = $('.navbar').css('height');
-      $('.navbar').animate({top: '-' + navHeight}, 150);
-      lastScrollTop = scrollTop;
-    } else if (lastScrollTop - scrollTop > 50) {
-      $('.navbar').animate({top: '0px'}, 150);
-      lastScrollTop = scrollTop;
-    }
-  });
+
 });
