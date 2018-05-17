3bbd819ae60d111e152b67ef1e05851de133831c
Phoebelisabethy <phoebe.hurst@hotmail.com>
2018-05-17 09:08:31 +0100

    Fork on to Bootstrap 4

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,4 +11,20 @@ $(document).ready(function(){
   });
 
 
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 1100);
+    return false;
+});
+
+$('.your-class').slick({
+         infinite: true,
+         slidesToShow: 3,
+         slidesToScroll: 3,
+         arrows: true,
+         dots: true,
+
+    });
+
 });
